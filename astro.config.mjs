7968c2a9a55d192d6f8e6f5a3b4431115586e415
GitHub Token User <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>
2026-02-04 03:37:55 +0000

    Include full Astro source code, configuration, and build

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://romorris.github.io',
+  base: '/Petes-Marine-Service2',
+  build: {
+    assets: 'assets'
+  }
+});
